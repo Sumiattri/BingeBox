72b99425bf8224c47c9ba7799dcb936770b866b2
Sumit Attri <sumitattri165@gmail.com>
2025-06-16 13:25:15 +0530

    routing setup done

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,46 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-import LandingPage from "./pages/Visiter/LandingPage";
+import {
+  Home,
+  Movies,
+  TvShows,
+  Games,
+  MyList,
+  LandingPage,
+  Login,
+  Signup,
+  ProtectedRoute,
+  HomeLayout,
+} from ".";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<Landing />} />
+      <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+
+      <Route
+        path="/home"
+        element={
+          <ProtectedRoute>
+            <HomeLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<Home />} />
+        <Route path="tv-shows" element={<TvShows />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="my-list" element={<MyList />} />
+        <Route path="games" element={<Games />} />
+      </Route>
     </>
   )
 );
 
 function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Authenticated Pages
+export { default as Home } from "./pages/User/Home";
+export { default as TvShows } from "./pages/User/TvShows";
+export { default as Movies } from "./pages/User/Movies";
+export { default as Games } from "./pages/User/Games";
+export { default as MyList } from "./pages/User/MyList";
+
+// Visitor Pages
+export { default as LandingPage } from "./pages/Visitor/LandingPage";
+export { default as Login } from "./pages/Visitor/Login";
+export { default as Signup } from "./pages/Visitor/Signup";
+
+//protected-Route
+export { default as ProtectedRoute } from "./routes/ProtectedRoute";
+
+//layout
+export { default as HomeLayout } from "./Layout/HomeLayout";
