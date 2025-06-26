4f9cb151503f51dbcd1edac84c0e25a25bb47b5f
Sumit Attri <sumitattri165@gmail.com>
2025-06-27 01:40:06 +0530

    changed trending

--- api/trending.js
+++ api/trending.js
@@ -23,34 +23,3 @@ export default async function handler(req, res) {
     res.status(500).json({ error: "Internal server error" });
   }
 }
-
-
-export default async function handler(req, res) {
-  try {
-    const response = await fetch("https://api.themoviedb.org/3/discover/movie?with_original_language=hi&sort_by=popularity.deschttps://api.themoviedb.org/3/discover/movie?with_original_language=hi&sort_by=popularity.desc&primary_release_date.gte=2024-04-18",
- {
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${process.env.TMDB_TOKEN}`,
-      },
-    });
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json({ error: "Popular fetch failed" });
-  }
-}
-export default async function handler(req, res) {
-  try {
-    const response = await fetch("https://api.themoviedb.org/3/discover/movie", {
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${process.env.TMDB_TOKEN}`,
-      },
-    });
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json({ error: "Discover fetch failed" });
-  }
-}
