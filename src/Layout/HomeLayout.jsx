61e30047aa46bbd4581724d0711c3ad3f0115885
Sumit Attri <sumitattri165@gmail.com>
2025-06-28 12:57:06 +0530

    removed footer overlap while loading

--- src/Layout/HomeLayout.jsx
+++ src/Layout/HomeLayout.jsx
@@ -8,7 +8,10 @@ function HomeLayout() {
     <div className="min-h-screen w-screen bg-[#141414]">
       <div className="absolute top-0 w-full h-[70px] bg-gradient-to-b from-black/50 via-black/40  to-[#141414] "></div>
       <Navbar />
-      <Outlet />
+      <div className="min-h-[100vh]">
+        {" "}
+        <Outlet />
+      </div>
       <Footer />
     </div>
   );
