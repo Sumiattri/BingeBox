6507401308cad2a091c3a6a7b6801f339e20374d
Sumit Attri <sumitattri165@gmail.com>
2025-06-29 15:18:13 +0530

    changed firebase app

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD3hDSImqvxvrsYgNm2NaHe6v52EvNIrCM",
+//   authDomain: "netflix-a7946.firebaseapp.com",
+//   projectId: "netflix-a7946",
+//   storageBucket: "netflix-a7946.firebasestorage.app",
+//   messagingSenderId: "704958908730",
+//   appId: "1:704958908730:web:f25f3f8a91598ae408e803",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD3hDSImqvxvrsYgNm2NaHe6v52EvNIrCM",
-  authDomain: "netflix-a7946.firebaseapp.com",
-  projectId: "netflix-a7946",
-  storageBucket: "netflix-a7946.firebasestorage.app",
-  messagingSenderId: "704958908730",
-  appId: "1:704958908730:web:f25f3f8a91598ae408e803",
+  apiKey: "AIzaSyAGEY0Uy6ZMzOcAALKYLz7pa9FcQbMFnEY",
+  authDomain: "netflix-2-3a381.firebaseapp.com",
+  projectId: "netflix-2-3a381",
+  storageBucket: "netflix-2-3a381.firebasestorage.app",
+  messagingSenderId: "638466750396",
+  appId: "1:638466750396:web:4447ae9c3cfc5bb2929db9",
 };
 
 // Initialize Firebase
